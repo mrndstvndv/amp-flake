9435d7c9171dd0b50a2853299359eb4c1007bd1c
mrndstvndv <stvndvmrnd@gmail.com>
2026-01-07 23:41:13 +0000

    initial comnit

---
+++ flake.nix
@@ -0,0 +1,93 @@
+{
+  description = "Amp CLI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      version = "0.0.1767816090-gfc80fb";
+
+      mkAmp = system: pkgs:
+        let
+          platformInfo = {
+            "aarch64-darwin" = {
+              platform = "darwin-arm64";
+              sha256 = "95bd668920b2c417fc9f6bf6fcd3863112bb78748ee907db31a7d9f13e3ccc4f";
+            };
+            "x86_64-darwin" = {
+              platform = "darwin-x64";
+              sha256 = "7b8d9b7772a42ae7376d7c787ea254e5f6dde7518374197eaa15f0e5c9d9956f";
+            };
+            "aarch64-linux" = {
+              platform = "linux-arm64";
+              sha256 = "dde5658f1ed84a6569fb0756d675cde8df33bd333c5604102091f82915cc21d3";
+            };
+            "x86_64-linux" = {
+              platform = "linux-x64";
+              sha256 = "1e4c3be160bbd47ef0ef5a37278ae1933c9cb4a3c99baa99139d8b190b730ced";
+            };
+          }.${system};
+          isLinux = pkgs.lib.hasSuffix "linux" system;
+        in
+        pkgs.stdenv.mkDerivation {
+          pname = "amp";
+          inherit version;
+
+          src = pkgs.fetchurl {
+            url = "https://storage.googleapis.com/amp-public-assets-prod-0/cli/0.0.1767816090-gfc80fb/amp-${platformInfo.platform}";
+            sha256 = platformInfo.sha256;
+          };
+
+          dontUnpack = true;
+          dontStrip = true;
+
+          nativeBuildInputs = pkgs.lib.optionals isLinux [ pkgs.patchelf ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src $out/bin/amp
+            chmod +w $out/bin/amp
+            chmod +x $out/bin/amp
+          '' + pkgs.lib.optionalString isLinux ''
+            patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) $out/bin/amp
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Amp CLI";
+            homepage = "https://ampcode.com";
+            license = licenses.unfree;
+            mainProgram = "amp";
+          };
+        };
+
+    in
+    {
+      packages.aarch64-darwin.default = mkAmp "aarch64-darwin" nixpkgs.legacyPackages.aarch64-darwin;
+      packages.aarch64-darwin.amp = self.packages.aarch64-darwin.default;
+      packages.x86_64-darwin.default = mkAmp "x86_64-darwin" nixpkgs.legacyPackages.x86_64-darwin;
+      packages.x86_64-darwin.amp = self.packages.x86_64-darwin.default;
+      packages.aarch64-linux.default = mkAmp "aarch64-linux" nixpkgs.legacyPackages.aarch64-linux;
+      packages.aarch64-linux.amp = self.packages.aarch64-linux.default;
+      packages.x86_64-linux.default = mkAmp "x86_64-linux" nixpkgs.legacyPackages.x86_64-linux;
+      packages.x86_64-linux.amp = self.packages.x86_64-linux.default;
+
+      apps.aarch64-darwin.default = {
+        type = "app";
+        program = "${self.packages.aarch64-darwin.default}/bin/amp";
+      };
+      apps.x86_64-darwin.default = {
+        type = "app";
+        program = "${self.packages.x86_64-darwin.default}/bin/amp";
+      };
+      apps.aarch64-linux.default = {
+        type = "app";
+        program = "${self.packages.aarch64-linux.default}/bin/amp";
+      };
+      apps.x86_64-linux.default = {
+        type = "app";
+        program = "${self.packages.x86_64-linux.default}/bin/amp";
+      };
+    };
+}
